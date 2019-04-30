f35f0f77c2e9ea99d7bd9bba6351236e5af22ba7
Tylor Lake <tylake@microsoft.com>
2019-04-30 11:43:59 -0700

    Adding a configurationsource for use in ASP

---
+++ Services/DataX.Config/DataX.Config.ServiceFabric/Extensions/Configuration/ServiceFabricConfigurationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿// *********************************************************************
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License
+// *********************************************************************
+
+namespace DataX.Config.ServiceFabric.Extensions.Configuration
+{
+    using Microsoft.Extensions.Configuration;
+    using System;
+
+    public static class ServiceFabricConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddInMemoryCollection(this IConfigurationBuilder configurationBuilder, string packageName)
+        {
+            if (configurationBuilder == null)
+            {
+                throw new ArgumentNullException("configurationBuilder");
+            }
+            configurationBuilder.Add(new ServiceFabricConfigurationSource(packageName));
+            return configurationBuilder;
+        }
+    }
+}
---
+++ Services/DataX.Config/DataX.Config.ServiceFabric/ServiceFabricConfigurationSource.cs
@@ -0,0 +1,35 @@
+﻿// *********************************************************************
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License
+// *********************************************************************
+
+namespace DataX.Config.ServiceFabric
+{
+    using Microsoft.Extensions.Configuration.Memory;
+    using System.Collections.Generic;
+    using System.Fabric;
+
+    public class ServiceFabricConfigurationSource : MemoryConfigurationSource
+    {
+        public ServiceFabricConfigurationSource(string packageName)
+        {
+            var package = FabricRuntime.GetActivationContext().GetConfigurationPackageObject(packageName);
+
+            this.InitialData = GetFlatConfig(package);
+        }
+
+        private IEnumerable<KeyValuePair<string,string>> GetFlatConfig(ConfigurationPackage package)
+        {
+            var flatConfig = new Dictionary<string, string>();
+            foreach (var section in package.Settings.Sections)
+            {
+                foreach (var parameter in section.Parameters)
+                {
+                    flatConfig[$"{section.Name}:{parameter.Name}"] = parameter.Value;
+                }
+            }
+
+            return flatConfig;
+        }
+    }
+}
