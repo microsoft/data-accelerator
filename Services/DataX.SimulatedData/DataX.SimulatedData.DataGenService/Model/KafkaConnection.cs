a9f4deb12f6626b960ae65063caa2db777f858e9
Rohit Agrawal <rohiagra@microsoft.com>
2019-05-01 00:15:52 -0700

    add support to simulate data into kafka

---
+++ Services/DataX.SimulatedData/DataX.SimulatedData.DataGenService/Model/KafkaConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataX.SimulatedData.DataGenService.Model
+{
+    public class KafkaConnection
+    {
+        public string Broker { get; set; }
+        public List<string> Topics { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
