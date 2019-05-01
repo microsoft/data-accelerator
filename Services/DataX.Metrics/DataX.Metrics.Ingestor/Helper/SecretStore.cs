924816bbafdef06439a9dbe607e30b1ad27aa300
tylorhl <tylorhl@users.noreply.github.com>
2019-05-01 08:43:06 -0700

    Reducing surface area of SF config calls

--- Services/DataX.Metrics/DataX.Metrics.Ingestor/Helper/SecretStore.cs
+++ Services/DataX.Metrics/DataX.Metrics.Ingestor/Helper/SecretStore.cs
@@ -1,7 +1,8 @@
-// *********************************************************************
+﻿// *********************************************************************
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License
 // *********************************************************************
+using DataX.Contract;
 using DataX.ServiceHost.ServiceFabric;
 using DataX.Utilities.KeyVault;
 using System;
@@ -27,21 +28,23 @@ namespace DataX.Metrics.Ingestor.Helper
 
         public async Task<string> GetMetricsEventHubListenerConnectionStringAsync()
         {
-            return await GetSecretAsync(Utility.GetConfigValue("EventhubNamespaceConnectionstring"));
+            return await GetSecretAsync(ServiceFabricUtil.GetServiceFabricConfigSetting("EventhubNamespaceConnectionstring"));
         }
 
         public async Task<string> GetMetricsStorageConnectionStringAsync()
         {
-            return await GetSecretAsync(Utility.GetConfigValue("StorageAccountConnectionstring"));
+            return await GetSecretAsync(ServiceFabricUtil.GetServiceFabricConfigSetting("StorageAccountConnectionstring"));
         }
         
         public async Task<string> GetMetricsRedisConnectionStringAsync()
         {
-            return await GetSecretAsync(Utility.GetConfigValue("RedisCacheConnectionstring"));
+            return await GetSecretAsync(ServiceFabricUtil.GetServiceFabricConfigSetting("RedisCacheConnectionstring"));
         }
 
-        private async Task<string> GetSecretAsync(string key)
+        private async Task<string> GetSecretAsync(ApiResult setting)
         {
+            string key = setting.Result?.ToString();
+
             KeyVaultManager keyManager = new KeyVaultManager();
             var secret = await keyManager.GetSecretStringAsync(_keyVaultName, key);
             return secret;
