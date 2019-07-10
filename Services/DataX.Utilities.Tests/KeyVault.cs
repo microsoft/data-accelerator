46223030c820ce17f85c13952143543b084a1ae5
Carl Brochu <cbrochu@microsoft.com>
2019-07-10 12:42:34 -0700

    commit merge

---
+++ Services/DataX.Utilities.Tests/KeyVault.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataX.Utilities.Tests
+{
+    [TestClass]
+    public class KeyVault
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Services/DataX.Utilities/DataX.Utilities.KeyVault/SecretUriParser.cs
@@ -0,0 +1,51 @@
+﻿// *********************************************************************
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License
+// *********************************************************************
+using System;
+using System.Text.RegularExpressions;
+
+namespace DataX.Utility.KeyVault
+{
+    public static class SecretUriParser
+    {
+        private static Regex _Reg = new Regex(@"^((keyvault|secretscope:?):\/\/)?([^:\/\s]+)(\/)(.*)?", RegexOptions.IgnoreCase);
+
+        public static void ParseSecretUri(string secretUri, out string keyvaultName, out string secretName)
+        {
+            if (!TryParseSecretUri(secretUri, out keyvaultName, out secretName))
+            {
+                throw new ArgumentException($"cannot parse {secretUri}");
+            }
+        }
+
+        public static bool TryParseSecretUri(string secretUri, out string keyvaultName, out string secretName)
+        {
+            if (secretUri == null)
+            {
+                keyvaultName = null;
+                secretName = null;
+                return false;
+            }
+
+            MatchCollection m = _Reg.Matches(secretUri);
+            if (m == null || m.Count == 0)
+            {
+                keyvaultName = null;
+                secretName = null;
+                return false;
+            }
+            else
+            {
+                keyvaultName = m[0].Groups[3].Value;
+                secretName = m[0].Groups[5].Value;
+                return true;
+            }
+        }
+
+        public static string ComposeUri(string keyvaultName, string secretName, string uriPrefix)
+        {
+            return $"{uriPrefix}://{keyvaultName}/{secretName}";
+        }
+    }
+}
