b8f2cf67d29afebd3eff5624f6e8b53547038c2a
kjcho <kjcho@microsoft.com>
2019-09-03 13:20:26 -0700

    add optimization in webpack config

--- Website/Website/webpack.config.js
+++ Website/Website/webpack.config.js
@@ -153,7 +153,32 @@ function configure(env, argv) {
             fs: 'empty',
             net: 'empty',
             tls: 'empty'
-        }
+        },
+
+        optimization: {
+            splitChunks: {
+              chunks: 'async',
+              minSize: 30000,
+              maxSize: 0,
+              minChunks: 1,
+              maxAsyncRequests: 5,
+              maxInitialRequests: 3,
+              automaticNameDelimiter: '~',
+              automaticNameMaxLength: 30,
+              name: true,
+              cacheGroups: {
+                vendors: {
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: -10
+                },
+                default: {
+                  minChunks: 2,
+                  priority: -20,
+                  reuseExistingChunk: true
+                }
+              }
+            }
+          }
     };
 
     if (!isDebug) {
