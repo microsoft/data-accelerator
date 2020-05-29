b67c607e04ccc29d9ee4780e3460f7f1644fead0
Rohit Agrawal - MSFT <rohiagra@microsoft.com>
2020-05-29 13:09:45 -0700

    Dispose objects (#170)
    * Dispose objects
    
    * Add using

--- Services/DataX.Utilities/DataX.Utilities.Blob/BlobStorage.cs
+++ Services/DataX.Utilities/DataX.Utilities.Blob/BlobStorage.cs
@@ -14,32 +14,42 @@ namespace DataX.Utilities.Blob
     {
         public static void UploadConfigsToBlobStorage(string subscriptionId, string keyvaultName, string resourceGroupName, string defaultLocation, string storageAccountName, string containerName, string fileName, string content, string clientId, string tenantId, string secretPrefix)
         {
-            AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix);
-            storageUtility.UploadConfigsToBlobStorage(resourceGroupName, storageAccountName, containerName, fileName, content);
+            using (AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix))
+            {
+                storageUtility.UploadConfigsToBlobStorage(resourceGroupName, storageAccountName, containerName, fileName, content);
+            }
         }
 
         public static void DeleteConfigFromBlobStorage(string subscriptionId, string keyvaultName, string resourceGroupName, string defaultLocation, string storageAccountName, string containerName, string fileName, string clientId, string tenantId, string secretPrefix)
         {
-            AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix);
-            storageUtility.DeleteConfigFromBlobStorage(resourceGroupName, storageAccountName, containerName, fileName);
+            using (AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix))
+            {
+                storageUtility.DeleteConfigFromBlobStorage(resourceGroupName, storageAccountName, containerName, fileName);
+            }
         }
 
         public static void DeleteAllConfigsFromBlobStorage(string subscriptionId, string keyvaultName, string resourceGroupName, string defaultLocation, string storageAccountName, string containerName, string containerPath, string clientId, string tenantId, string secretPrefix)
         {
-            AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix);
-            storageUtility.DeleteAllConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, containerPath).Wait();
+            using (AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix))
+            {
+                storageUtility.DeleteAllConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, containerPath).Wait();
+            }
         }
 
         public static string LoadConfigFromBlobStorage(string subscriptionId, string keyvaultName, string resourceGroupName, string defaultLocation, string storageAccountName, string containerName, string fileName, string clientId, string tenantId, string secretPrefix)
         {
-            AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix);
-            return storageUtility.LoadConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, fileName);
+            using(AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix))
+            {
+                return storageUtility.LoadConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, fileName);
+            }
         }
 
         public static List<string> LoadAllConfigsFromBlobStorage(string subscriptionId, string keyvaultName, string resourceGroupName, string defaultLocation, string storageAccountName, string containerName, string containerPath, string fileName, string clientId, string tenantId, string secretPrefix)
         {
-            AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix);
-            return storageUtility.LoadAllConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, containerPath, fileName);
+            using (AzureStorageUtility storageUtility = new AzureStorageUtility(subscriptionId, defaultLocation, keyvaultName, clientId, tenantId, secretPrefix))
+            {
+                return storageUtility.LoadAllConfigsFromBlobStorage(resourceGroupName, storageAccountName, containerName, containerPath, fileName);
+            }
         }
     }
 }
