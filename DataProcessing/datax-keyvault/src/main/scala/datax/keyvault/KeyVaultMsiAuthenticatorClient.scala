774b7d6dddda0560cd05f4d5bc2c3022bd59ecb1
Rohit Agrawal - MSFT <rohiagra@microsoft.com>
2021-10-06 12:29:19 -0700

    Switch from KeyVaultClient to SecretClient and remove sqldb dependency (#194)
    * Switch from KeyVaultClient to SecretClient and remove sqldb dependency
    
    * throw exception when data is sent to SQLDB

--- DataProcessing/datax-keyvault/src/main/scala/datax/keyvault/KeyVaultMsiAuthenticatorClient.scala
+++ DataProcessing/datax-keyvault/src/main/scala/datax/keyvault/KeyVaultMsiAuthenticatorClient.scala
@@ -4,30 +4,23 @@
 // *********************************************************************
 package datax.keyvault
 
-import com.microsoft.azure.keyvault.KeyVaultClient
-import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials
+import com.azure.core.credential.{AccessToken,TokenCredential,TokenRequestContext}
+import com.azure.security.keyvault.secrets.{SecretClient,SecretClientBuilder}
+import java.time.OffsetDateTime
 import org.json4s.DefaultFormats
 import org.json4s.JsonAST.JNothing
 import org.json4s.jackson.JsonMethods.parse
+import reactor.core.publisher.Mono
 
-object KeyVaultMsiAuthenticatorClient {
+class SecretClientTokenCredential extends TokenCredential {
 
   // This is the local endpoint on which the HDInsight Cluster nodes will listen to for MSI access requests
   private def localMsiEndpoint="http://localhost:40381/managed/identity/oauth2/token"
 
-  // Get keyVaultClient that is authenticated with MSI
-  def getKeyVaultClient(): KeyVaultClient={
-
-    val keyVaultClient = new KeyVaultClient(
-      new KeyVaultCredentials() {
-        override def doAuthenticate(authorization: String, resource: String, scope: String): String = {
-          return getAccessToken("https://vault.azure.net")
-        }
-      }
-    )
-    keyVaultClient
+  def getToken(request:TokenRequestContext): Mono[AccessToken] = {
+		Mono.just(new AccessToken(getAccessToken("https://vault.azure.net"), OffsetDateTime.now().plusMinutes(5)))	
   }
-
+  
   // Get the access token for the passed in MSI resource by calling into the local endpoint
   private def getAccessToken(resourceId:String):String ={
 
@@ -45,5 +38,18 @@ object KeyVaultMsiAuthenticatorClient {
     }
 
     tokenstr
+  }
+}
+
+object KeyVaultMsiAuthenticatorClient {
+
+  // Get keyVaultClient that is authenticated with MSI
+  def getKeyVaultClient(keyvaultName:String): SecretClient ={
+
+    val secretClient  = new SecretClientBuilder()
+            .vaultUrl(s"https://$keyvaultName.vault.azure.net")
+			.credential(new SecretClientTokenCredential())
+            .buildClient()
+    secretClient 
   }
 }
