9ba24869234b6284f4359aca190d459efb33f2fe
jozavala <jozavala@microsoft.com>
2022-07-21 15:38:26 -0700

    Log additional details

--- DataProcessing/datax-core/src/main/scala/datax/service/TelemetryService.scala
+++ DataProcessing/datax-core/src/main/scala/datax/service/TelemetryService.scala
@@ -4,7 +4,10 @@
 // *********************************************************************
 package datax.service
 
+import java.sql.Timestamp
+
 trait TelemetryService {
   def trackEvent(event: String, properties: Map[String, String], measurements: Map[String, Double])
   def trackException(e: Exception, properties: Map[String, String], measurements: Map[String, Double])
+  def trackBatchEvent(event: String, properties: Map[String, String], measurements: Map[String, Double], batchTime: Timestamp)
 }
