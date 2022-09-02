fb688d2e13561c1d78982c57d9c64fa73388135b
Yinyu Wang <95396482+yinyuwang@users.noreply.github.com>
2022-09-02 03:41:36 +0000

    add timeout for AccessToken (#522)

--- DataProcessing/datax-core/src/main/scala/datax/authentication/ManagedIdentity.scala
+++ DataProcessing/datax-core/src/main/scala/datax/authentication/ManagedIdentity.scala
@@ -7,9 +7,11 @@ package datax.authentication
 import org.json4s.DefaultFormats
 import org.json4s.JsonAST.JNothing
 import org.json4s.jackson.JsonMethods.parse
+import org.apache.log4j.LogManager
 
 object ManagedIdentity {
   private def localMsiEndpoint="http://localhost:40381/managed/identity/oauth2/token"
+  def logger = LogManager.getLogger("ManagedIdentity")
 
   // Get the access token for the passed in MSI resource by calling into the local endpoint
   def getAccessToken(resourceId:String): String = {
@@ -17,7 +19,9 @@ object ManagedIdentity {
     implicit val formats: DefaultFormats.type = DefaultFormats
 
     val endpointId = s"$localMsiEndpoint?resource=$resourceId&api-version=2018-11-01"
+    logger.warn("getAccessToken start")
     val token = HttpGetter.httpGet(endpointId, Option(Map("Metadata"->"true")))
+    logger.warn(s"getAccessToken end")
     val tokenJson = parse(token)
 
     var tokenstr =""
