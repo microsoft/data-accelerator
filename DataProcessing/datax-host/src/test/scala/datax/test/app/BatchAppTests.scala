13b39c277c4926b8a9fcea77c08bb1b2226be79e
REDMOND\jozavala <jozavala@microsoft.com>
2023-10-04 11:59:00 -0700

    Refactor and cleanup

--- DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
+++ DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
@@ -12,9 +12,10 @@ class BatchAppTests extends FlatSpec with Matchers with PrivateMethodTester with
     setEnv("process_start_datetime", "2023-10-03T00:00:00Z")
     setEnv("process_end_datetime", "2023-10-03T00:59:59Z")
     BatchApp.create(Array(
-      "conf=classpath:test.conf",
+      "conf=classpath:datax/tests/test1/test.conf",
       "partition=false",
       "batchflushmetricsdelayduration=1 seconds",
       "filterTimeRange=false"), createTestSparkSession())
+
   }
 }
