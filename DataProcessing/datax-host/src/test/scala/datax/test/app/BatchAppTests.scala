5a856435d6dd44458a3d7c391587ffb558ccd279
REDMOND\jozavala <jozavala@microsoft.com>
2023-10-09 16:27:01 -0700

    Enable partition=true test scenario

--- DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
+++ DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
@@ -7,28 +7,33 @@ import org.json4s.jackson.JsonMethods.render
 import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.json4s.jackson.parseJson
 
 import java.time.Instant
 import java.util.TimeZone
 
 class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester with SparkSessionTestWrapper {
 
-  case class TestCase(inputArgs: Array[String], blobData: String, projectionData: String, transformData: String, schemaData: String) {
+  case class TestCase(inputArgs: Array[String], envVars: Array[(String, String)], blobs: Array[(String, String)], projectionData: String, transformData: String, schemaData: String) {
     lazy val Timestamp = Instant.now().toEpochMilli
     lazy val Directory = FileUtils.getTempDirectory
     lazy val InputDir = s"${Directory.getPath}/datax/${Timestamp}/input"
     lazy val OutputDir = s"${Directory.getPath}/datax/${Timestamp}/output"
 
+    envVars.foreach(envVar => setEnv(envVar._1, envVar._2))
+    writeBlobs()
+
     def getConfig(): String = {
       s"""
          |datax.job.name=test
          |datax.job.input.default.blobschemafile=${encodeToBase64String(schemaData)}
-         |datax.job.input.default.blobpathregex=.*/\\d{4}/\\d{2}/\\d{2}/\\d{2}$$
+         |datax.job.input.default.blobpathregex=.*/input/(\\d{4})/(\\d{2})/(\\d{2})/(\\d{2})/.*$$
          |datax.job.input.default.filetimeregex=(\\d{4}/\\d{2}/\\d{2}/\\d{2})$$
+         |datax.job.input.default.sourceidregex=file:/.*/(input)/.*
          |datax.job.input.default.blob.test.path=${InputDir}/{yyyy/MM/dd/HH}/
          |datax.job.input.default.blob.test.partitionincrement=1
          |datax.job.input.default.blob.test.compressiontype=none
-         |datax.job.input.default.source.testinput.target=testoutput
+         |datax.job.input.default.source.input.target=output
          |datax.job.output.default.blob.group.main.folder=${OutputDir}
          |datax.job.output.default.blob.compressiontype=none
          |datax.job.process.transform=${encodeToBase64String(transformData)}
@@ -36,12 +41,12 @@ class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester w
          |""".stripMargin
     }
 
-    def writeBlobs(partition: String): Unit = { // 2023/10/03/00
+    def writeBlobs(): Unit = {
       var i = 0
-      blobData.trim().split("\n").foreach(blob => {
-        writeFile(s"${InputDir}/${partition}/blob_$i.json", blob.trim())
+      blobs.foreach(blobData => blobData._2.trim().split("\n").foreach(blob => {
+        writeFile(s"${InputDir}/${blobData._1}/blob_$i.json", blob.trim())
         i += 1
-      })
+      }))
     }
 
     def getInputArgs(): Array[String] = {
@@ -64,10 +69,13 @@ class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester w
   TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
 
   lazy val test1 = TestCase(
-    blobData =
-      """
-        |{ "name": "hi" }
-        |""".stripMargin,
+    blobs = Array(
+      ("2023/10/03/00", """
+        |{ "name": "hello" }
+        |""".stripMargin),
+      ("2023/10/03/00", """
+        |{ "name": "world" }
+        |""".stripMargin)),
     projectionData =
       """
         |__DataX_FileInfo
@@ -94,21 +102,19 @@ class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester w
         |}
         |""".stripMargin,
     inputArgs = Array(
-      "partition=false",
+      "partition=true",
       "batchflushmetricsdelayduration=1 seconds",
       "filterTimeRange=false"
+    ),
+    envVars = Array(
+      ("process_start_datetime", "2023-10-03T00:00:00Z"),
+      ("process_end_datetime", "2023-10-03T01:59:59Z")
     )
   )
   "BatchApp" should "process correctly a single blob running in local machine" in {
-    test1.writeBlobs(partition = "2023/10/03/00")
-    setEnv("process_start_datetime", "2023-10-03T00:00:00Z")
-    setEnv("process_end_datetime", "2023-10-03T00:59:59Z")
     BatchApp.main(test1.getInputArgs())
-    val jsonVal = test1.readBlob("0.json")
     implicit val formats = org.json4s.DefaultFormats
-    val output = org.json4s.jackson
-      .parseJson(jsonVal)
-    val nameValue = render(output \\ "Raw" \\ "name").extract[String]
-    assert("hi" == nameValue)
+    assert("hello" == render(parseJson(test1.readBlob("0.json")) \\ "Raw" \\ "name").extract[String])
+    assert("world" == render(parseJson(test1.readBlob("1.json")) \\ "Raw" \\ "name").extract[String])
   }
 }
