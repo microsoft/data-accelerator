1901be94df8361aec9e3215c512ca65bfd02fac8
REDMOND\jozavala <jozavala@microsoft.com>
2023-10-10 03:55:47 -0700

    Refactor + make unit tests deterministic

--- DataProcessing/datax-host/src/main/scala/datax/app/LocalBatchApp.scala
+++ DataProcessing/datax-host/src/main/scala/datax/app/LocalBatchApp.scala
@@ -1,69 +1,45 @@
 package datax.app
 
+import datax.utility.ArgumentsParser
 import org.apache.commons.io.FileUtils
 import org.apache.commons.lang3.StringUtils
 
 import java.io.File
 import java.time.Instant
+import java.util.Base64.getEncoder
 import java.util.TimeZone
 
-case class LocalBatchApp(inputArgs: Array[String], envVars: Array[(String, String)], blobs: Array[(String, String)], projectionData: String, transformData: String, schemaData: String, additionalSettings: String = "") {
-
-  TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
-
-  lazy val Timestamp = Instant.now().toEpochMilli
-  lazy val Directory = FileUtils.getTempDirectory
-  lazy val InputDir = s"${Directory.getPath}/datax/${Timestamp}/input"
-  lazy val OutputDir = s"${Directory.getPath}/datax/${Timestamp}/output"
-  lazy val LocalModeInputArgs = Array(
-    "spark.master=local",
-    "spark.hadoop.fs.file.impl=com.globalmentor.apache.hadoop.fs.BareLocalFileSystem",
-    "spark.hadoop.fs.azure.test.emulator=true",
-    "spark.hadoop.fs.azure.storage.emulator.account.name=devstoreaccount1.blob.windows.core.net")
-
-  envVars.foreach(envVar => setEnv(envVar._1, envVar._2))
-  writeBlobs()
-
-  def getConfig(): String = {
-    s"""
-       |datax.job.name=test
-       |datax.job.input.default.blobschemafile=${encodeToBase64String(schemaData)}
-       |datax.job.input.default.blobpathregex=.*/input/(\\d{4})/(\\d{2})/(\\d{2})/(\\d{2})/.*$$
-       |datax.job.input.default.filetimeregex=(\\d{4}/\\d{2}/\\d{2}/\\d{2})$$
-       |datax.job.input.default.sourceidregex=file:/.*/(input)/.*
-       |datax.job.input.default.source.input.target=output
-       |datax.job.input.default.blob.input.path=${InputDir}/{yyyy/MM/dd/HH}/
-       |datax.job.output.default.blob.group.main.folder=${OutputDir}
-       |datax.job.process.transform=${encodeToBase64String(transformData)}
-       |datax.job.process.projection=${encodeToBase64String(projectionData)}
-       |""".stripMargin.trim() + "\n" + additionalSettings.trim()
+/**
+ * Encoder for configuration string supplied values
+ */
+trait ConfigValueEncoder {
+  def encodeValue(src: String): String = {
+    new String(getEncoder.encode(src.getBytes()))
   }
+}
 
-  def writeBlobs(): Unit = {
-    var i = 0
-    blobs.foreach(blobData => blobData._2.trim().split("\n").foreach(blob => {
-      writeFile(s"${InputDir}/${blobData._1}/blob_$i.json", blob.trim())
-      i += 1
-    }))
-  }
+/**
+ * File system that supports the Local App
+ */
+trait LocalAppFileSystem {
+  val InputDir: String
+  val OutputDir: String
 
-  def getInputArgs(): Array[String] = {
-    LocalModeInputArgs ++ Array(s"conf=${encodeToBase64String(getConfig())}") ++ inputArgs
-  }
+  def fileExists(fsFileName: String): Boolean
 
-  def readOutputBlob(blobFileName: String, outputPartition: String = ""): String = {
-    val blobFullPath = if(StringUtils.isEmpty(outputPartition)) s"$OutputDir/$blobFileName" else s"$OutputDir/$outputPartition/$blobFileName"
-    assert(fileExists(blobFullPath))
-    val jsonVal = readFile(blobFullPath)
-    jsonVal
-  }
+  def readFile(fsFileName: String): String
 
-  def setEnv(key: String, value: String) = {
-    val field = System.getenv().getClass.getDeclaredField("m")
-    field.setAccessible(true)
-    val map = field.get(System.getenv()).asInstanceOf[java.util.Map[java.lang.String, java.lang.String]]
-    map.put(key, value)
-  }
+  def writeFile(path: String, data: String): Unit
+}
+
+/**
+ * Local File system implementation
+ */
+case class LocalAppLocalFileSystem() extends LocalAppFileSystem {
+  val Timestamp: String = Instant.now().toEpochMilli.toString
+  val Directory: String = FileUtils.getTempDirectory.getPath
+  val InputDir = s"${Directory}/datax/${Timestamp}/input"
+  val OutputDir = s"${Directory}/datax/${Timestamp}/output"
 
   def fileExists(fsFileName: String): Boolean = {
     new File(fsFileName).exists()
@@ -73,15 +49,123 @@ case class LocalBatchApp(inputArgs: Array[String], envVars: Array[(String, Strin
     FileUtils.readFileToString(new File(fsFileName))
   }
 
-  def encodeToBase64String(src: String) = {
-    new String(java.util.Base64.getEncoder.encode(src.getBytes()))
-  }
-
-  def writeFile(path: String, data: String) = {
+  def writeFile(path: String, data: String): Unit = {
     FileUtils.write(new File(path), data)
   }
+}
 
-  def main(inputArguments: Array[String] = Array.empty): Unit = {
+/**
+ * Configuration supplied to input parameter -conf
+ */
+trait LocalConfiguration {
+  def getConfig(fs: LocalAppFileSystem): String
+}
+
+/**
+ * The configuration is supplied via string values
+ * @param jobName The job name
+ * @param projectionData The projection data file content
+ * @param transformData The transform data file content
+ * @param schemaData The schema data file content
+ * @param additionalSettings Additional properties from conf files
+ */
+case class ValueConfiguration(jobName: String, projectionData: String, transformData: String, schemaData: String, additionalSettings: String = "") extends LocalConfiguration with ConfigValueEncoder {
+  def getConfig(fs: LocalAppFileSystem): String = {
+    encodeValue(s"""
+       |datax.job.name=$jobName
+       |datax.job.input.default.blobschemafile=${encodeValue(schemaData)}
+       |datax.job.input.default.blobpathregex=.*/input/(\\d{4})/(\\d{2})/(\\d{2})/(\\d{2})/.*$$
+       |datax.job.input.default.filetimeregex=(\\d{4}/\\d{2}/\\d{2}/\\d{2})$$
+       |datax.job.input.default.sourceidregex=file:/.*/(input)/.*
+       |datax.job.input.default.source.input.target=output
+       |datax.job.input.default.blob.input.path=${fs.InputDir}/{yyyy/MM/dd/HH}/
+       |datax.job.output.default.blob.group.main.folder=${fs.OutputDir}
+       |datax.job.process.transform=${encodeValue(transformData)}
+       |datax.job.process.projection=${encodeValue(projectionData)}
+       |""".stripMargin.trim() + "\n" + additionalSettings.trim())
+  }
+}
+
+case class LocalBatchApp(inputArgs: Array[String], configuration: Option[LocalConfiguration] = None, blobs: Array[(String, String)] = Array(), envVars: Array[(String, String)] = Array(), fs: LocalAppFileSystem = LocalAppLocalFileSystem()) {
+
+  lazy val LocalModeInputArgs = Array(
+    "spark.master=local",
+    "spark.hadoop.fs.file.impl=com.globalmentor.apache.hadoop.fs.BareLocalFileSystem",
+    "spark.hadoop.fs.azure.test.emulator=true",
+    "spark.hadoop.fs.azure.storage.emulator.account.name=devstoreaccount1.blob.windows.core.net")
+
+  /**
+   * Writes the value supplied blobs into the backed up file system
+   */
+  def writeBlobs(): Unit = {
+    if(!blobs.isEmpty) {
+      var i = 0
+      blobs.foreach(blobData => blobData._2.trim().split("\n").foreach(blob => {
+        fs.writeFile(s"${fs.InputDir}/${blobData._1}/blob_$i.json", blob.trim())
+        i += 1
+      }))
+    }
+  }
+
+  /**
+   * Expands the list of arguments on top of the caller ones to initialize local mode behavior and configuration generation
+   * @return
+   */
+  def getInputArgs(): Array[String] = {
+    LocalModeInputArgs ++
+      (configuration.map(conf => Array(s"conf=${conf.getConfig(fs)}")).getOrElse(Array.empty)) ++
+      inputArgs
+  }
+
+  /**
+   * Read a blob from the output directory partition
+   * @param blobFileName The blob file name
+   * @param outputPartition The partition within the output directory
+   * @return
+   */
+  def readOutputBlob(blobFileName: String, outputPartition: String = ""): String = {
+    val blobFullPath = if(StringUtils.isEmpty(outputPartition)) s"${fs.OutputDir}/$blobFileName" else s"${fs.OutputDir}/$outputPartition/$blobFileName"
+    assert(fs.fileExists(blobFullPath))
+    fs.readFile(blobFullPath)
+  }
+
+  /**
+   * Sets a environment variable programatically
+   * @param key The enviroment variable name
+   * @param value The environment variable value
+   * @return
+   */
+  def setEnv(key: String, value: String) = {
+    val field = System.getenv().getClass.getDeclaredField("m")
+    field.setAccessible(true)
+    val map = field.get(System.getenv()).asInstanceOf[java.util.Map[java.lang.String, java.lang.String]]
+    map.put(key, value)
+  }
+
+  /**
+   * Entrypoint
+   */
+  def main(): Unit = {
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"))
+    if(!envVars.isEmpty) {
+      envVars.foreach(envVar => setEnv(envVar._1, envVar._2))
+    }
+    if(!blobs.isEmpty) {
+      writeBlobs()
+    }
     BatchApp.main(getInputArgs())
+  }
+}
+
+/**
+ * Companion object to expose a static main method
+ */
+object LocalBatchApp {
+  /**
+   * Entry point for a LocalBatchApp when called externally via class reference
+   * @param inputArguments The input arguments array
+   */
+  def main(inputArguments: Array[String]): Unit = {
+    LocalBatchApp(inputArgs = inputArguments).main()
   }
 }
--- DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
+++ DataProcessing/datax-host/src/test/scala/datax/test/app/BatchAppTests.scala
@@ -1,6 +1,6 @@
 package datax.test.app
 
-import datax.app.LocalBatchApp
+import datax.app.{LocalBatchApp, ValueConfiguration}
 import org.json4s.jackson.JsonMethods.render
 import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpec
@@ -9,39 +9,55 @@ import org.json4s.jackson.parseJson
 
 class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester {
 
-  lazy val testApp = LocalBatchApp(
-    blobs = Array(
-      ("2023/10/03/00", """
+import scala.collection.mutable
+
+  lazy val testApp = LocalBatchApp(blobs = Array(
+    ("2023/10/03/00",
+      """
         |{ "name": "hello" }
         |""".stripMargin),
-      ("2023/10/03/01", """
+    ("2023/10/03/01",
+      """
         |{ "name": "world" }
+        |""".stripMargin),
+    ("2023/10/03/02",
+      """
+        |{ "name": "!" }
         |""".stripMargin)),
-    projectionData =
-      """
-        |__DataX_FileInfo
-        |__DataXMetadata_OutputPartitionTime
-        |Raw
-        |DataXProperties
-        |""".stripMargin,
-    transformData =
-      """
-        |default = SELECT * FROM DataXProcessedInput
-        |""".stripMargin,
-    schemaData =
-      """
-        |{
-        |  "type": "struct",
-        |  "fields": [
-        |    {
-        |      "name": "name",
-        |      "type": "string",
-        |      "nullable": true,
-        |      "metadata": {}
-        |    }
-        |  ]
-        |}
-        |""".stripMargin,
+    configuration = Some(ValueConfiguration(
+      jobName = "test",
+      projectionData =
+        """
+          |__DataX_FileInfo
+          |__DataXMetadata_OutputPartitionTime
+          |Raw
+          |DataXProperties
+          |""".stripMargin,
+      transformData =
+        """
+          |default = SELECT * FROM DataXProcessedInput
+          |""".stripMargin,
+      schemaData =
+        """
+          |{
+          |  "type": "struct",
+          |  "fields": [
+          |    {
+          |      "name": "name",
+          |      "type": "string",
+          |      "nullable": true,
+          |      "metadata": {}
+          |    }
+          |  ]
+          |}
+          |""".stripMargin,
+      additionalSettings =
+        """
+          |datax.job.input.default.blob.input.partitionincrement=1
+          |datax.job.input.default.blob.input.compressiontype=none
+          |datax.job.output.default.blob.compressiontype=none
+          |""".stripMargin
+    )),
     inputArgs = Array(
       "partition=true",
       "batchflushmetricsdelayduration=1 seconds",
@@ -49,19 +65,21 @@ class BatchAppTests extends AnyFlatSpec with Matchers with PrivateMethodTester {
     ),
     envVars = Array(
       ("process_start_datetime", "2023-10-03T00:00:00Z"),
-      ("process_end_datetime", "2023-10-03T01:59:59Z")
-    ),
-    additionalSettings =
-      """
-        |datax.job.input.default.blob.input.partitionincrement=1
-        |datax.job.input.default.blob.input.compressiontype=none
-        |datax.job.output.default.blob.compressiontype=none
-        |""".stripMargin
+      ("process_end_datetime", "2023-10-03T02:59:59Z")
+    )
   )
   "BatchApp" should "process correctly a single blob running in local machine" in {
     testApp.main()
     implicit val formats = org.json4s.DefaultFormats
-    assert(Array("hello", "world").contains(render(parseJson(testApp.readOutputBlob("0.json")) \\ "Raw" \\ "name").extract[String]))
-    assert(Array("hello", "world").contains(render(parseJson(testApp.readOutputBlob("1.json")) \\ "Raw" \\ "name").extract[String]))
+    val results = Array[String](
+      render(parseJson(testApp.readOutputBlob("0.json")) \\ "Raw" \\ "name").extract[String],
+      render(parseJson(testApp.readOutputBlob("1.json")) \\ "Raw" \\ "name").extract[String],
+      render(parseJson(testApp.readOutputBlob("2.json")) \\ "Raw" \\ "name").extract[String]
+    )
+    val expected = mutable.Queue[String]("hello", "world", "!")
+    while(expected.nonEmpty) {
+      val check = expected.dequeue()
+      assert(results.contains(check))
+    }
   }
 }
