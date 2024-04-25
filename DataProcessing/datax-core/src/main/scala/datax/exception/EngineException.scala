40b0d1e904d04f130838389a1c4ee112475ea6d5
Edward Maeng <smaeng@microsoft.com>
2024-04-25 13:48:26 -0700

    Delete DataProcessing

--- DataProcessing/datax-core/src/main/scala/datax/exception/EngineException.scala
+++
@@ -1,8 +0,0 @@
-// *********************************************************************
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// Licensed under the MIT License
-// *********************************************************************
-package datax.exception
-
-case class EngineException(private val message: String = "", private val cause: Throwable = None.orNull)
-  extends Exception(message, cause)
--- DataProcessing/datax-host/src/main/scala/datax/client/eventhub/EventHubConf.scala
+++
@@ -1,7 +0,0 @@
-// *********************************************************************
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// Licensed under the MIT License
-// *********************************************************************
-package datax.client.eventhub
-
-case class EventHubConf(name: String, connectionString: String)
